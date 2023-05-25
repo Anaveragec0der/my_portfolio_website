e70ab4bcfcd75b7960adaea121c3732dcdd904d6
Priyanshu <naveragec0der@gmail.com>
2023-05-25 06:48:27 +0530

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <section className="min-h-fit flex flex-col justify-start items-center py-13 px-5 text-center">
+        <section className="min-h-fit flex flex-col justify-start items-center py-14 px-5 text-center">
             <p className="max-w-xl font-light text-gray-500">Made with ❤️ </p>
         </section>
     )
