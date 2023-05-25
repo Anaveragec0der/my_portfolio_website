c2d4ab4c89e9d16ba864789e57fdd8962c79fb4a
Priyanshu <naveragec0der@gmail.com>
2023-05-25 13:51:14 +0530

    setting dark mode by default

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-  const[darkMode,setDarkMode]=useState(false)
+  const[darkMode,setDarkMode]=useState(true)
   return (
     <div className={darkMode && "dark"}> 
       <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
