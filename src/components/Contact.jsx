aa6c75e26529745dcb81f0e92acfee5dd14245d1
Priyanshu <naveragec0der@gmail.com>
2023-05-25 13:45:11 +0530

    adding gmail contact option

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,24 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaGoogle } from "react-icons/fa";
 import Section from "./common/Section";
 import contact from "../assets/mobile.png";
 
 export default function Contact() {
-
   const SOCIAL = [
-      {
-        id: 1,
-        link: "https://linkedin.com",
-        icon: <FaLinkedin />,
-      },
+    {
+      id: 1,
+      link: "https://www.linkedin.com/in/priyanshu-vishwakarma-63b54423b/",
+      icon: <FaLinkedin />,
+    },
     {
       id: 2,
-      link: "https://github.com",
+      link: "https://github.com/Anaveragec0der",
       icon: <FaGithub />,
     },
-
   ];
+  const gmail = {
+    id: 2,
+    icon: <FaGoogle />,
+  };
 
   return (
     <Section
@@ -28,7 +30,11 @@ export default function Contact() {
       <div className="flex flex-col items-center justify-center gap-8 text-center">
         <div>
           {" "}
-          <img src={contact} alt=" contact-information" className="w-32 h-32 ml-7" />{" "}
+          <img
+            src={contact}
+            alt=" contact-information"
+            className="w-32 h-32 ml-7"
+          />{" "}
         </div>
         <div>
           <p className="text-sm md-text-base max-w-xs md:max-w-lg font-extralight">
@@ -39,40 +45,81 @@ export default function Contact() {
         <div className="flex w-full items-center justify-evenly text-3xl">
           {SOCIAL.map(({ id, link, icon }) => {
             return (
-              <a key={id} href={link} target="_blank" rel="noopener noreferrer" className="duration-200 ease-in-out hover:text-rose-600">
+              <a
+                key={id}
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="duration-200 ease-in-out hover:text-rose-600"
+              >
                 {icon}
               </a>
             );
           })}
+          <a
+            key={gmail.id}
+            href="mailto:priyanshuvishwakarma445@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {gmail.icon}
+          </a>
         </div>
         {/* {bottom form} */}
         <div className="p-8 text-left w-full flex items-center justify-center">
-            <form action="https://getform.io/f/a458405c-f648-412a-a374-6eb676e2809e" 
-            method="POST">
-                <div className="gap-4 w-full">
-                    <div className="flex flex-col">
-                        <label className="capitalize text-sm py-2 font-extralight">Name</label>
-                        <input type="text" name="Name" className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"/>
-                    </div>
-                    <div className="flex flex-col my-2">
-                        <label className="capitalize text-sm py-2 font-extralight">Phone</label>
-                        <input type="text" name="Phone" className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"/>
-                    </div>
-                    <div className="flex flex-col my-2">
-                        <label className="capitalize text-sm py-2 font-extralight">Email</label>
-                        <input  type="text" name="Email" className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"/>
-                    </div>
-                    <div className="flex flex-col">
-                        <label className="capitalize text-sm py-2 font-extralight">Message</label>
-                        <textarea  rows="10" name="Message" className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white resize-none"></textarea>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center">
-                    <button   className="my-8 bg-gradient-to-r from-rose-600 to-teal-500 text-white px-6 py-3 uppercase rounded-md tracking-wider cursor-pointer hover:scale-105 duration-200">Send Message</button>
-                </div>
-            </form>
+          <form
+            action="https://getform.io/f/a458405c-f648-412a-a374-6eb676e2809e"
+            method="POST"
+          >
+            <div className="gap-4 w-full">
+              <div className="flex flex-col">
+                <label className="capitalize text-sm py-2 font-extralight">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="Name"
+                  className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                />
+              </div>
+              <div className="flex flex-col my-2">
+                <label className="capitalize text-sm py-2 font-extralight">
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="Phone"
+                  className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                />
+              </div>
+              <div className="flex flex-col my-2">
+                <label className="capitalize text-sm py-2 font-extralight">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="Email"
+                  className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="capitalize text-sm py-2 font-extralight">
+                  Message
+                </label>
+                <textarea
+                  rows="10"
+                  name="Message"
+                  className="border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white resize-none"
+                ></textarea>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <button className="my-8 bg-gradient-to-r from-rose-600 to-teal-500 text-white px-6 py-3 uppercase rounded-md tracking-wider cursor-pointer hover:scale-105 duration-200">
+                Send Message
+              </button>
+            </div>
+          </form>
         </div>
-        
       </div>
     </Section>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,63 +1,84 @@
-import {FaGithub,FaLinkedin,FaArrowDown} from "react-icons/fa"
-import avatar from "../assets/avatar.png"
-export default function Hero(){
-    const SOCIAL=[
-        {
-            id:1,
-            link:"https://linkedin.com",
-            icon:<FaLinkedin/>
-        },
-        {
-            id:2,
-            link:"https://github.com",
-            icon:<FaGithub/>
-        },
-    ]
-    window.addEventListener('scroll',function(){
-        const downArrow=document.querySelector('.down-arrow')
-        if(this.scrollY>=150 && window.innerWidth > 768) downArrow.classList.add('hide-down-arrow')
-        else downArrow.classList.remove('hide-down-arrow')
-    })
-    return(
-        <section className="min-h-screen flex flex-col justify-start items-center p-5 text-center">
-            <h2 className="text-5xl text-rose-600 uppercase font-bold">Priyanshu Vishwakarma</h2>
-            <h3 className="py-3 text-2xl">Full Stack Developer</h3>
-            <p className="max-w-xl font-light text-gray-500">
-                Hello <span className="animate-pulse text-4xl">👋</span>,
-                Welcome to my portfolio
-                I'm a Full stack developer.
-                I hold a Master's degree in Computer Applications. 
-                Problem solving is something that has always intrigued me
-                and it is what made me start programming.
-                I also love making vector arts, 
-                it is what made me pick up full stack development as
-                a carrer path because now I can be creative when I am working
-                at the front end and can solve problems when I'm at the backend
-                thus getting best of both the worlds 
-            </p>
+import { FaGithub, FaLinkedin, FaGoogle, FaArrowDown } from "react-icons/fa";
+import avatar from "../assets/avatar.png";
+export default function Hero() {
+  const SOCIAL = [
+    {
+      id: 1,
+      link: "https://www.linkedin.com/in/priyanshu-vishwakarma-63b54423b/",
+      icon: <FaLinkedin />,
+    },
+    {
+      id: 2,
+      link: "https://github.com/Anaveragec0der",
+      icon: <FaGithub />,
+    },
+  ];
+  const gmail = {
+    id: 2,
+    icon: <FaGoogle />,
+  };
+  window.addEventListener("scroll", function () {
+    const downArrow = document.querySelector(".down-arrow");
+    if (this.scrollY >= 150 && window.innerWidth > 768)
+      downArrow.classList.add("hide-down-arrow");
+    else downArrow.classList.remove("hide-down-arrow");
+  });
+  return (
+    <section className="min-h-screen flex flex-col justify-start items-center p-5 text-center">
+      <h2 className="text-5xl text-rose-600 uppercase font-bold">
+        Priyanshu Vishwakarma
+      </h2>
+      <h3 className="py-3 text-2xl">Full Stack Developer</h3>
+      <p className="max-w-xl font-light text-gray-500">
+        Hello <span className="animate-pulse text-4xl">👋</span>, Welcome to my
+        portfolio I'm a Full stack developer. I hold a Master's degree in
+        Computer Applications. Problem solving is something that has always
+        intrigued me and it is what made me start programming. I also love
+        making vector arts, it is what made me pick up full stack development as
+        a carrer path because now I can be creative when I am working at the
+        front end and can solve problems when I'm at the backend thus getting
+        best of both the worlds
+      </p>
 
-            {/* {social icons} */}
-            <div className="flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3">
-            {SOCIAL.map(({ id, link, icon }) => {
-            return (
-              <a key={id} href={link} target="_blank" rel="noopener noreferrer">
-                {icon}
-              </a>
-            );
-          })}
+      {/* {social icons} */}
+      <div className="flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3">
+        {SOCIAL.map(({ id, link, icon }) => {
+          return (
+            <a key={id} href={link} target="_blank" rel="noopener noreferrer">
+              {icon}
+            </a>
+          );
+        })}
+        <a
+          key={gmail.id}
+          href="mailto:priyanshuvishwakarma445@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {gmail.icon}
+        </a>
 
-                {/* avatar and resume */}
+        {/* avatar and resume */}
+      </div>
+      <div>
+        <img
+          src={avatar}
+          alt="avatar"
+          className="scale-110 w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b from-rose-600 rounded-2xl pt-5"
+        />
+        <a
+          href="/Priyanshu'sResume.pdf"
+          download={true}
+          className="flex items-center justify-center mt-10 bg-gradient-to-r from-rose-600 to-teal-500 text-white py-2 rounded-lg"
+        >
+          Resume
+        </a>
+      </div>
 
-            </div>
-            <div>
-                <img src={avatar} alt="avatar" className="scale-110 w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b from-rose-600 rounded-2xl pt-5"/>
-                <a href="/Priyanshu'sResume.pdf" download={true} className="flex items-center justify-center mt-10 bg-gradient-to-r from-rose-600 to-teal-500 text-white py-2 rounded-lg">Resume</a>
-            </div>
-
-            {/* arrow down animation */}
-            <div className="mt-10 down-arrow">
-                <FaArrowDown className="text-gray-400 text-2xl animate-bounce"/>
-            </div>
-        </section>
-    )
+      {/* arrow down animation */}
+      <div className="mt-10 down-arrow">
+        <FaArrowDown className="text-gray-400 text-2xl animate-bounce" />
+      </div>
+    </section>
+  );
 }
